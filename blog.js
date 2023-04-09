862da755c44518d323bae400a155c20700e70e51
Fathima Nashva <nashvakabeer17@gmail.com>
2023-04-09 17:52:40 +0530

    database added but not showing in db

--- blog.js
+++ blog.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-/*var mongoose = require('mongoose');
-mongoose.connect("mongoose://127.0.0.1/blogdb");*/
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1/blogdb");
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -12,6 +12,15 @@ app.set('views','./view');
 app.use(express.static('blog/css'));
 app.use(express.static('blog/js'));
 app.use(express.static('blog/images'));
+
+var userSchema = mongoose.Schema({
+    name: String,
+    mailid: String,
+    phone: Number,
+    password: String,
+    createdAt: {type: Date, default: Date.now}
+});
+
 
 var signup = require('./signin.js')
 
